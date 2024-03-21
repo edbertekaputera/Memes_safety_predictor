88211f31510ba3041872910e979bd90777ed5e58
Carlos <c.frasenda@yahoo.com>
2024-03-22 00:29:29 +0800

    ocr stuff

--- components/main.py
+++ components/main.py
@@ -5,16 +5,17 @@ import cv2
 from langdetect import detect
 import pytesseract
 import preprocess_image as ppImg
-from googletrans import Translator
+import translate_image as trnsImg
+
+import easyocr
 
 
 # pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract'
 
 if __name__ == "__main__":
 
-    img_path = "../test/images/zhongguo.jpg"
+    img_path = '../test/images/tamil4.jpg'
     image = Image.open(img_path)
-
 
     #transform iamge
     preprocessor = ppImg.PreprocessImage(metrics=['grayscale','remove_noise'])
@@ -22,16 +23,26 @@ if __name__ == "__main__":
 
     #extract text
     converted_image = Image.fromarray(image_np)
-    converted_image.show()
 
-    text = pytesseract.image_to_string(converted_image, lang='chi_sim')
+    #detect language
+    script_name, _ = trnsImg.detect_language(img_path)
+
+    print(script_name)
+
+    if script_name == "Han":
+        text = pytesseract.image_to_string(converted_image, lang='chi_sim')
+    elif script_name == "Tamil":
+        text = pytesseract.image_to_string(converted_image, lang='tam')
+    elif script_name == "Arabic":
+        text1 = pytesseract.image_to_string(converted_image, lang='chi_sim')
+        text2 = pytesseract.image_to_string(converted_image, lang='tam')
+        if len(text1) < len(text2):
+            text = text1
+        else:
+            text = text2
+    else:
+        text = pytesseract.image_to_string(converted_image)
     
     # Print the extracted text
     print("Extracted Text:")
     print(text)
-
-    #translate text
-    translator = Translator()
-    translate_text = translator.translate(text) 
-
-    print(translate_text.text)
--- components/translate_image.py
+++ components/translate_image.py
@@ -1,9 +1,13 @@
 from PIL import Image
 import pytesseract
+import re
 from transformers import MarianMTModel, MarianTokenizer
 from googletrans import Translator
+import cv2 as cv
 
 import os
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract'
 
 def load_images(directory):
     img_paths_list = []
@@ -19,3 +23,17 @@ def convert_text(text):
     translate_text = translator.translate(text) 
 
     return translate_text
+
+
+def detect_language(img_path):
+    try:
+        original_image = cv.imread(img_path)
+        resized_image = cv.resize(original_image, (int(int(original_image.shape[1]) * 5), int(int(original_image.shape[1]) * 5)))
+        osd = pytesseract.image_to_osd(resized_image)
+        script = re.search("Script: ([a-zA-Z]+)\n", osd).group(1)
+        conf = re.search("Script confidence: (\d+\.?(\d+)?)", osd).group(1)
+        return script, float(conf)
+    except Exception as e: 
+        print (e)
+        return "Han", 0.0
+
