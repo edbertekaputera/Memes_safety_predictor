d97171dc6e80e9bb751b2a20d6730e6ebc7fe73d
Carlos <c.frasenda@yahoo.com>
2024-03-22 17:52:38 +0800

    functionized extract

--- components/extract_text.py
+++ components/extract_text.py
@@ -6,7 +6,7 @@ import translate_image as trnsImg
 def extractText(img_path):
     #extract image
     image = Image.open(img_path)
-    
+
     #transform iamge
     preprocessorBasic = ppImg.PreprocessImage(metrics=['grayscale','bilateral','thresholding'])
     image_np = preprocessorBasic.transform_image(image)
@@ -22,13 +22,9 @@ def extractText(img_path):
 
     #extract text
     converted_image = Image.fromarray(image_np)
-    converted_image.show()
     converted_imageEng = Image.fromarray(image_npEng)
-    converted_imageEng.show()
     converted_imageChi = Image.fromarray(image_npChi)
-    converted_imageChi.show()
     converted_imageTam = Image.fromarray(image_npTam)
-    converted_imageTam.show()
 
     #detect language
     script_name, _ = trnsImg.detect_language(img_path)
@@ -37,27 +33,30 @@ def extractText(img_path):
     #select language
     if script_name == "Han":
         text = pytesseract.image_to_string(converted_imageChi, lang='chi_sim')
+        lang = "chi_sim"
     elif script_name == "Tamil":
         text = pytesseract.image_to_string(converted_imageTam, lang='tam')
+        lang = "Tam"
     elif script_name == "Arabic":
         text1 = pytesseract.image_to_string(converted_imageChi, lang='chi_sim')
         text2 = pytesseract.image_to_string(converted_imageTam, lang='tam')
         text3 = pytesseract.image_to_string(converted_image, lang='eng')
         if len(text1) < len(text2)/1.5:
-            print("Tamil")
             if len(text3) < 3*len(text2):
                 text = text2
+                lang = "Tam"
             else:
-                print('eng')
                 text = text3
+                lang = "eng"
         else:
-            print("CHI")
             if len(text3) < len(text1):
                 text = text1
+                lang = "chi_sim"
             else:
-                print('eng')
                 text = text3
+                lang = "eng"
     else:
         text = pytesseract.image_to_string(converted_imageEng, lang='eng')
+        lang = "eng"
     
-    return text
+    return text, lang
